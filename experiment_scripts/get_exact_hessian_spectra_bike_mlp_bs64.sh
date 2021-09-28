d3090266909856af1a2aea260d77c75ce1cc8fa4
Nick Baskerville <>
2021-09-28 20:44:18 +0100

    Regression option in bike script.

--- experiment_scripts/get_exact_hessian_spectra_bike_mlp_bs64.sh
+++ experiment_scripts/get_exact_hessian_spectra_bike_mlp_bs64.sh
@@ -15,4 +15,4 @@ cd /home/jr19127/nn-spectral-spacings/GadamX/rmt
 # PBS ARRAY INDEX is 1-up, but bash arrays are 0-up, so leading pad with -1
 
 # Execute code
-python get_exact_hessian_spectra.py --data_path $WORK/data/Bike --model MLP_Bike --dataset Bike --checkpoint=$WORK/nn_spectral_models/Bike_MLPBike/MLP_Bike/SGD/seed\=1_lr\=0.003_mom\=0.9_wd\=0.0005_batchsize\=128_numepochs\=300/checkpoint-00300.pt --out $WORK/nn_spectral_models/hessian_spectra/Bike/MLP/bs64.hdf5
+python get_exact_hessian_spectra.py --data_path $WORK/data/Bike --model MLP_Bike --dataset Bike --checkpoint=$WORK/nn_spectral_models/Bike_MLPBike/MLP_Bike/SGD/seed\=1_lr\=0.003_mom\=0.9_wd\=0.0005_batchsize\=128_numepochs\=300/checkpoint-00300.pt --out $WORK/nn_spectral_models/hessian_spectra/Bike/MLP/bs64.hdf5 -regression
