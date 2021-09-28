74f2c30d48d52154222e7ccafdd7cc138700a21b
Nick Baskerville <>
2021-09-28 20:37:53 +0100

    Added bike spectral script and changed compute requirements.

--- experiment_scripts/get_exact_hessian_spectra_bike_mlp_bs64.sh
+++ experiment_scripts/get_exact_hessian_spectra_bike_mlp_bs64.sh
@@ -14,7 +14,5 @@ cd /home/jr19127/nn-spectral-spacings/GadamX/rmt
 
 # PBS ARRAY INDEX is 1-up, but bash arrays are 0-up, so leading pad with -1
 
-
-
 # Execute code
-python get_exact_hessian_spectra.py --data_path $WORK/data/cifar10 --model LeNet --dataset CIFAR10 --checkpoint=$WORK/nn_spectral_models/CIFAR10/LeNet/SGD/checkpoint-00300.pt --out $WORK/nn_spectral_models/hessian_spectra/CIFAR10/LeNet/bs64_test.hdf5 -test
+python get_exact_hessian_spectra.py --data_path $WORK/data/Bike --model MLP_Bike --dataset Bike --checkpoint=$WORK/nn_spectral_models/Bike_MLPBike/MLP_Bike/SGD/seed\=1_lr\=0.003_mom\=0.9_wd\=0.0005_batchsize\=128_numepochs\=300/checkpoint-00300.pt --out $WORK/nn_spectral_models/hessian_spectra/Bike/MLP/bs64.hdf5
