dac8c170c99d04bc53ad1d39008d610882bee83f
Nick Baskerville <>
2021-10-02 10:27:29 +0100

    fp16 in script

--- experiment_scripts/get_exact_hessian_spectra_cifar_lenet_bs64_test.sh
+++ experiment_scripts/get_exact_hessian_spectra_cifar_lenet_bs64_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=8:mem=500gb
+#PBS -l select=1:ncpus=8:ngpus=1:mem=100gb
 
 
 module load lang/gcc/9.1.0 
@@ -17,4 +17,4 @@ cd /home/jr19127/nn-spectral-spacings/GadamX/rmt
 
 
 # Execute code
-python get_exact_hessian_spectra.py --data_path $WORK/data/cifar10 --model LeNet --dataset CIFAR10 --checkpoint=$WORK/nn_spectral_models/CIFAR10/LeNet/SGD/checkpoint-00300.pt --out $WORK/nn_spectral_models/hessian_spectra/CIFAR10/LeNet/bs64_test.hdf5 -test
+python get_exact_hessian_spectra.py --data_path $WORK/data/cifar10 --model LeNet --dataset CIFAR10 --checkpoint=$WORK/nn_spectral_models/CIFAR10/LeNet/SGD/checkpoint-00300.pt --out $WORK/nn_spectral_models/hessian_spectra/CIFAR10/LeNet/bs64_test.hdf5 -test -fp16
