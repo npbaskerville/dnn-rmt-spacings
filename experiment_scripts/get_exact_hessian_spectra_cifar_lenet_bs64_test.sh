0a563a9a00b9cdae58228ac569e86d5f76666375
Nick Baskerville <>
2021-10-11 22:34:40 +0100

    Updated lenet spectra script

--- experiment_scripts/get_exact_hessian_spectra_cifar_lenet_bs64_test.sh
+++ experiment_scripts/get_exact_hessian_spectra_cifar_lenet_bs64_test.sh
@@ -17,4 +17,4 @@ cd /home/jr19127/nn-spectral-spacings/GadamX/rmt
 
 
 # Execute code
-python get_exact_hessian_spectra.py --data_path $WORK/data/cifar10 --model LeNet --dataset CIFAR10 --checkpoint=$WORK/nn_spectral_models/CIFAR10/LeNetsmall/SGD/checkpoint-00300.pt --out $WORK/nn_spectral_models/hessian_spectra/CIFAR10/LeNetsmall/bs64_test.hdf5 -test -fp16
+python get_exact_hessian_spectra.py --data_path $WORK/data/cifar10 --model LeNet --dataset CIFAR10 --checkpoint=/work/jr19127/nn_spectral_models/CIFAR10/LeNetsmallCIFAR10/LeNet/SGD/seed\=1_lr\=0.003_mom\=0.9_wd\=0.0005_batchsize\=128_numepochs\=300/checkpoint-00300.pt --out $WORK/nn_spectral_models/hessian_spectra/CIFAR10/LeNetsmall/bs64_test.hdf5 -test 
